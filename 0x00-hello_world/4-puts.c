9342dff42ab9677eac7c1c60e9bce1afe7d32a26
Superboyjay91 <emmijay2017@gmail.com>
2023-02-18 08:24:48 -0800

    4put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 
 /**
-<<<<<<< HEAD
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-  puts("\" Programming is like building a multilingual puzzle"\n);
-   	return (0);
-=======
  * main - Entry Point
  *
  * Return: Always 0 (Success)
@@ -20,5 +10,4 @@ int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
->>>>>>> e71f06cb3d255a227d289a09cc9856b531ec5097
 }
